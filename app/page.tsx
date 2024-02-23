72127a91416285d36dcbd80e9536d198ac4d6123
Gavin Gregory <gavin.a.gregory@gmail.com>
2024-02-23 13:51:23 -0800

    update text

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
           <p>I have been engrossed by the internet since I was in kindergarten.</p>
 
           <p>
-            My past almost half-decade professional years have been spent in Javascript and{' '}
+            My past almost half-decade of professional years have been spent in Javascript and{' '}
             <em>TypeScript</em>, mostly in <em>React</em>. I truly enjoy working within JavaScript,
             learning about JavaScript, and am simply how the web works. I can't imagine not working
             in TypeScript these days, even though it makes me feel dumber and dumber everyday. I
