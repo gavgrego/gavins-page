1c3dfadfd19b663e09a6ace3c4e86d7e7f2cc0f5
Gavin Gregory <gavin.a.gregory@gmail.com>
2024-04-04 10:22:43 +0700

    update homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -12,36 +12,40 @@ const Home = () => {
         </div>
 
         <div className="flex flex-col gap-8 text-xl">
-          <p>I have been engrossed by the internet since I was in kindergarten.</p>
-
           <p>
-            My past almost half-decade of professional years have been spent in Javascript and{' '}
-            <em>TypeScript</em>, mostly in <em>React</em>. I can't imagine not working in TypeScript
-            these days, even though it often makes me feel dumb. I truly enjoy working within
-            JavaScript, learning about JavaScript, and simply how the web works. I have also touched
-            so much else of <em>modern front-end tooling</em> ranging from Vue, Astro, NextJS,
-            Tailwind, GraphQL, various headless CMSs, and various component libraries.
+            I have been engrossed with how the internet works since kindergarten, spending my time
+            creating rudimentary websites as a child.
           </p>
 
           <p>
-            Before narrowing my focus on front-end software development, I spent five years managing
-            PHP-based websites, a time before the rise of React and contemporary front-end
-            development. While the majority of my experience is in front-end development, I also
-            have some experience in back-end development with Laravel, Go, and Node.
+            My past almost half-decade of professional years have been spent in Javascript and{' '}
+            <em>TypeScript</em>, mostly in <em>React</em>. I truly enjoy working within JavaScript,
+            learning about JavaScript, and how the web works as a whole. I have also touched so much
+            else of <em>modern front-end and full-stack tooling</em> ranging from <em>Next</em>,{' '}
+            <em>Vue</em>, <em>Go</em>, <em>Astro</em>, <em>Tailwind</em>, <em>GraphQL</em>, various{' '}
+            headless CMSs, various component libraries, and more.
+          </p>
+
+          <p>
+            Before narrowing my focus on modern, JavaScript-centered web development, I spent five
+            years managing PHP-based websites, a time before the rise of React and contemporary
+            front-end tooling. While the majority of my experience is in front-end development, I
+            also have experience in back-end with Laravel, Go, and Node.
           </p>
 
           <p>
             I add immense value not just as an engineer, but a team member. I pride myself on
             continuously improving, always showing up, and being an authentic person that peers
-            enjoy working with.
+            enjoy working with and having around. I am a strong communicator, a good listener, and
+            always striving to be better.
           </p>
 
           <div className="flex flex-row gap-4">
             <p className="bg-tie-dye bg-top rounded-md  font-semibold text-cream-100  text-sm p-4 leading-6 shadow-md">
               I have resided in the San Francisco Bay Area in beautiful Marin County with my wife
-              since the Summer of 2021, previously residing in Indianapolis. I love traveling,
-              writing, photography, spending time outside, going to concerts, and spinning up a side
-              project or two a year.
+              since the Summer of 2021, previously residing in Indianapolis, Indiana. I love
+              traveling, writing, photography, spending time outside, going to concerts, and
+              spinning up a side project or two a year.
             </p>
           </div>
 
